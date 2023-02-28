501a9a60a3e3958713a66b148e624faa7e6432dc
Adi Muhamad Fajar <adimuhamadfajar@gmail.com>
2023-02-28 09:09:17 +0700

    Pertemuan 1, 28 Feb 2023 Signed-off-by:Adi Muhamad Fajar <adimuhamadfajar@gmail.com>

---
+++ src/Pertemuan1/Mahasiswa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan1;
+
+/**
+ *
+ * @author ACER
+ */
+public class Mahasiswa {
+    String nama;
+    public static void main(String[] args) {
+        Mahasiswa mhs1= new Mahasiswa();
+        mhs1.nama="Adi Muhamad Fajar";
+        System.out.println(mhs1.nama);
+    }
+}
